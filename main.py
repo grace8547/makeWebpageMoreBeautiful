fddb8d1af6f3286c0e7b789064271378cdc960e5
DESKTOP-SNS0LL4\czcxc <chenxc.grace@gmail.com>
2017-09-03 14:21:50 -0700

    inital commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import bs4 as bs
+import urllib2
+import pandas as pd
+
+def ScrapCatInfo(result):
+  cats = urllib2.urlopen(result['href']).read()
+  s = bs.BeautifulSoup(cats, 'lxml')
+  print(s.get_text())
+
+
+sauce = urllib2.urlopen("http://www.adoptapet.com/cat-adoption/search/50/miles/94403?color=Calico+or+Dilute+Calico&color_id=50").read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+# results = soup.find('div',class_='results_wrapper')
+# array = pd.ndarray(cats)
+for result in soup.findAll('a', {'class': 'smaller_line_height'}): 
+  ScrapCatInfo(result)
+  break                                                   
+  # print(result['href'])
+  # cats[i] = urllib2.urlopen(result['href']).read()
+# for result in soup.findAll('a'):
+#   if result.parent.name == 'featured-thumbnail':
+#     print result["href"]
+
