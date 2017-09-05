824733b6c8368da1872bc5807826f6798f902015
KaiyuanZhu <kzhu4@stanford.edu>
2017-09-04 21:50:55 -0700

    multi-threading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import bs4 as bs
 import urllib2
-
-def ScrapCatInfo(result):
+import threading
+def GetCatStory(result):
   cats = urllib2.urlopen(result['href']).read()
   s = bs.BeautifulSoup(cats, 'lxml')
   storys = s.find('div', {'class':'hidden-xs body'}).parent.find_all('div')
@@ -13,13 +13,15 @@ def ScrapCatInfo(result):
 
 sauce = urllib2.urlopen("http://www.adoptapet.com/cat-adoption/search/50/miles/94403?color=Calico+or+Dilute+Calico&color_id=50").read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
-numCats = 5
 results = soup.findAll('span', {'class':'featured-thumbnail'})
-i=0
+threads = []
 for result in results:
-	ScrapCatInfo(result.find('a', {'class': 'smaller_line_height'}))
-	i=i+1
-  	# just primt numCats cats
-  	if i==numCats:
-  		break                                        
+	aTag = result.find('a', {'class': 'smaller_line_height'})
+	t=threading.Thread(target = GetCatStory, args = (aTag,))
+	threads.append(t)
+	t.start()
+
+for t in threads:
+	t.join()
+
 
