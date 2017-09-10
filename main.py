ffa0befbdd3c84fb0619bf6b32b249b76fbaa3fa
DESKTOP-SNS0LL4\czcxc <chenxc.grace@gmail.com>
2017-09-10 14:24:18 -0700

    shelter classify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import bs4 as bs
 import urllib2
-
+import re
 def ScrapCatInfo(result):
   single_cat_helper = urllib2.urlopen(result['href']).read()
   single_cat = bs.BeautifulSoup(single_cat_helper, 'lxml')
@@ -9,14 +9,17 @@ def ScrapCatInfo(result):
   # print(story[1].text)
   # print('')
 
-def ScrapShelterInfo(result):
+def ScrapShelterInfo(result, rescueGroup):
   single_shelter_helper = urllib2.urlopen(result['href']).read()
   single_shelter = bs.BeautifulSoup(single_shelter_helper, 'lxml')
   shelter = single_shelter.find('div', {'class': 'body contact_sidebar hidden-sm hidden-md hidden-lg'})
-  title = []
+
   if shelter is not None:
     for li in shelter.find_all('li'):
-      print (li.text)
+      if li.find('b', text=re.compile('Rescue Group')):
+        if not li.find('a').text in rescueGroup:
+          rescueGroup.append(li.find('a').text)
+  print(rescueGroup)
 
 
 source = urllib2.urlopen("http://www.adoptapet.com/cat-adoption/search/50/miles/94403?color=Calico+or+Dilute+Calico&color_id=50").read()
@@ -24,9 +27,10 @@ soup = bs.BeautifulSoup(source, 'lxml')
 numCats = 5
 results = soup.findAll('span', {'class':'featured-thumbnail'})
 i=0
+rescueGroup = []
 for result in results:
   ScrapCatInfo(result.find('a', {'class': 'smaller_line_height'}))
-  ScrapShelterInfo(result.find('a', {'class': 'smaller_line_height'}))
+  ScrapShelterInfo(result.find('a', {'class': 'smaller_line_height'}), rescueGroup)
   i=i+1
   if i==numCats:
     break                                        
